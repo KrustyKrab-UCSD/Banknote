e598394c104d53ff99f1523ce4c66378d1bb1d71
artballesteros <58927442+artballesteros@users.noreply.github.com>
2021-10-30 21:46:58 -0700

    finish navigation bar

--- app/src/main/java/com/example/banknote/MainActivity.java
+++ app/src/main/java/com/example/banknote/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.banknote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // uninitialize once fragments are set up
+                Fragment fragment = null;
+                // fragment names subject to change
+                switch (item.getItemId()) {
+                    case R.id.action_accounts:
+                        // fragment = new AccountsFragment();
+                        break;
+                    case R.id.action_analysis:
+                        // fragment = new AnalysisFragment();
+                        break;
+                    case R.id.action_settings:
+                        // fragment = new SettingsFragment();
+                        break;
+                    default:
+                        // fragment = new AccountsFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
     }
 }
